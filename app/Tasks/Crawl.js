dd2d4071a25faee9b016a0ac020ce4b56ec9c99a
hossein <mdoroud97@gmail.com>
2020-02-27 18:04:24 +0330

    add crawl with schedule

--- app/Tasks/Crawl.js
+++ app/Tasks/Crawl.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const Task = use('Task')
+const rp = require('request-promise')
+const Country = use('App/Models/Country')
 
 class Crawl extends Task {
   static get schedule () {
@@ -8,7 +10,33 @@ class Crawl extends Task {
   }
 
   async handle () {
-    this.info('Task Crawl handle')
+    const option = {
+      url:'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc&outSR=102100&resultOffset=0&resultRecordCount=100&cacheHint=true',
+      json: true
+    }
+
+    rp(option)
+      .then(async (data) => {
+        const list = data.features
+        for (let item of list) {
+          const name = item.attributes.Country_Region
+          const confirmed = item.attributes.Confirmed
+          const recovered = item.attributes.Recovered
+          const deaths = item.attributes.Recovered
+          await Country.findOrCreate(
+            { name: name },
+            { 
+              name: name,
+              confirmed: confirmed,
+              recovered: recovered,
+              deaths: deaths
+            }
+          )
+        }
+      })
+      .catch((err) => {
+        this.info(`error: ${err}`)
+      })
   }
 }
 
